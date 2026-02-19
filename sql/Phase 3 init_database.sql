3fdcf87b2c7b392a3ba7ad12a9a4831dd287daaa
Azab Basha <azabbayoumy@gmail.com>
2026-02-20 02:27:31 +0300

    Update database and schema creation script

--- sql/Phase 3 init_database.sql
+++ sql/Phase 3 init_database.sql
@@ -8,47 +8,51 @@ Run this on a SQL Server instance with a login that can create databases.
 USE master;
 GO
 
--- Create the DataWarehouse database if it doesn't exist
-IF DB_ID(N'DataWarehouse') IS NULL
+-- Create the AdventureWorks2025_CustomerDW database if it doesn't exist
+IF DB_ID(N'AdventureWorks2025_CustomerDW') IS NULL
 BEGIN
-    PRINT 'Creating database [DataWarehouse]...';
-    CREATE DATABASE [DataWarehouse];
-    PRINT 'Database created.';
+	PRINT 'Creating database AdventureWorks2025_CustomerDW...';
+	CREATE DATABASE AdventureWorks2025_CustomerDW;
+	PRINT 'Database AdventureWorks2025_CustomerDW created successfully.';
 END
 ELSE
 BEGIN
-    PRINT 'Database [DataWarehouse] already exists. Skipping creation.';
+	PRINT 'Database AdventureWorks2025_CustomerDW already exists. Skipping creation.';
 END
 GO
 
-USE [DataWarehouse];
+USE AdventureWorks2025_CustomerDW;
 GO
 
 -- Create schemas if they do not exist
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = N'bronze')
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'bronze')
 BEGIN
-    EXEC('CREATE SCHEMA [bronze];');
-    PRINT 'Created schema [bronze].';
+	PRINT 'Creating schema bronze...';
+	EXEC('CREATE SCHEMA bronze');
+	PRINT 'Schema bronze created successfully.';
 END
 ELSE
-    PRINT 'Schema [bronze] already exists; skipping.';
+	PRINT 'Schema bronze already exists. Skipping creation.';
 
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = N'silver')
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'silver')
 BEGIN
-    EXEC('CREATE SCHEMA [silver];');
-    PRINT 'Created schema [silver].';
+	PRINT 'Creating schema silver...';
+	EXEC('CREATE SCHEMA silver');
+	PRINT 'Schema silver created successfully.';
 END
 ELSE
-    PRINT 'Schema [silver] already exists; skipping.';
+	PRINT 'Schema silver already exists. Skipping creation.';
 
-IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = N'gold')
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'gold')
 BEGIN
-    EXEC('CREATE SCHEMA [gold];');
-    PRINT 'Created schema [gold].';
+	PRINT 'Creating schema gold...';
+	EXEC('CREATE SCHEMA gold');
+	PRINT 'Schema gold created successfully.';
 END
 ELSE
-    PRINT 'Schema [gold] already exists; skipping.';
+	PRINT 'Schema gold already exists. Skipping creation.';
 
 GO
 
 PRINT 'Done. Your DataWarehouse database with bronze/silver/gold schemas is ready.';
+
